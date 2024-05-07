8c22b3bb27d2277064960f22e038f790f12c2f68
serhii honcharenko <32honcharenko@gmail.com>
2024-05-07 14:48:37 +0300

    APIHW

---
+++ api-testing/users.test.ts
@@ -0,0 +1,38 @@
+import axios from "axios"
+import { describe } from "node:test"
+import jsonpath from "jsonpath"
+import fs from "fs-extra"
+
+let userName:String
+let userPass:String
+let authToken:String
+
+describe('test for users', () => {
+    test('get token', async () => {
+        const all_users_response = await axios.get("https://dummyjson.com/users")
+        console.log(all_users_response.data)
+
+        let userName = String(jsonpath.query(all_users_response.data, '$..users[0].username'))
+
+        let userPass = String(jsonpath.query(all_users_response.data, '$..users[0].password'))
+
+        console.log(userName)
+        console.log(userPass)
+
+        expect(all_users_response.status).toEqual(200)
+    })
+
+
+    test('get auth token', async () => {
+        const auth_token_response = await axios.post('https://dummyjson.com/auth/login')
+
+
+
+        authToken = String(jsonpath.query(auth_token_response.data, "$..token"))
+        console.log(authToken)
+
+        fs.writeJSONSync('api-token.json', authToken)
+    })
+})
+
+//$..users[?(@.id==12)].username
